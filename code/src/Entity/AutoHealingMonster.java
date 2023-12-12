7894e5659fff8c3425015378e9f6f5c2c45f8cbf
Cadiot dorian <E224021B@univ-nantes.fr>
2023-12-12 16:29:59 +0100

    Javadoc

--- code/src/Entity/AutoHealingMonster.java
+++ code/src/Entity/AutoHealingMonster.java
@@ -1,26 +1,27 @@
 package Entity;
 
-public class AutoHealingMonster extends Monster {
-   private int luck;
+public class AutoHealingMonster extends Monster {;
    private int healingCapacity;
 
     /**
      * Monstre qui a 20% de chance de se soigner d'un nombre de PV égal à healingCapacity
-     * @param HP
-     * @param baseAttack
-     * @param defense
-     * @param type
-     * @param reward
-     * @param luck
-     * @param healingCapacity
+     * @param HP entier representant les HP
+     * @param baseAttack entier representant les degats de base
+     * @param defense entier representant la defense
+     * @param type chaine de caractere representant le type de monstre
+     * @param reward entier representant la recompense
+     * @param healingCapacity entier représentant la regeneration du monstre
      */
-    public AutoHealingMonster (int HP, int baseAttack, int defense, String type, int reward, int luck, int healingCapacity) {
+    public AutoHealingMonster (int HP, int baseAttack, int defense, String type, int reward, int healingCapacity) {
         super (HP, baseAttack, defense, type, reward);
         this.luck = luck;
         this.healingCapacity = healingCapacity;
             
     }
 
+    /**
+     * donne une chance au monstre de se soigner
+     */
     public void mayHeal() {
         double luck = Math.random();
         if (luck > 0.8) {
@@ -31,9 +32,6 @@ public class AutoHealingMonster extends Monster {
         }
    }
 
-    public int getLuck() {
-        return luck;
-    }
    
     @Override
     public int getAttackPower() {
