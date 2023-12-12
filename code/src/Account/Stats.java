ebca366f32ce11bc76a09ed487b31fa015a1e07e
Cadiot dorian <E224021B@univ-nantes.fr>
2023-12-12 16:21:52 +0100

    Javadoc de Stats

--- code/src/Account/Stats.java
+++ code/src/Account/Stats.java
@@ -3,18 +3,33 @@ package Account;
 public class Stats {
     private int score;
 
+    /**
+     * Constructeur de Stats
+     */
     public Stats(){
         this.score = 0;
     }
 
+    /**
+     * 2 ème Constructeur de Stats
+     * @param score le score voulu
+     */
     public Stats(int score){
         this.score = score;
     }
 
+    /**
+     * Getter de score
+     * @return un entier représentat le score
+     */
     public int getScore() {
         return score;
     }
 
+    /**
+     * Setter de Score
+     * @param score un entier représentat le score
+     */
     public void setScore(int score) {
         this.score = score;
     }
